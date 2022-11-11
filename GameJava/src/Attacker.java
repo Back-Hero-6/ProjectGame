b9fc5674cf08ab60dcef18a29b78596dceb12f13
Gonzalo Piasek <>
2022-11-11 17:49:01 +0100

    version 2

--- GameJava/src/Attacker.java
+++ GameJava/src/Attacker.java
@@ -1,4 +1,9 @@
 public interface Attacker {
 
+    // ATTACKS
+    // HEAVY: damage heavyAttack = strength; stamina-5
+    // WEAK: damage=half strength (as int); stamina +1
+
+
     void attack(Character Character);
 }
