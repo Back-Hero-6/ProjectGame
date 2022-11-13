796f20efecb143ba7e14f8c0b5c2592668c10368
Javi <javiquiles93@gmail.com>
2022-11-13 20:45:39 +0100

    añado cosas al main

--- GameJava/src/Main.java
+++ GameJava/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido al Battle 1 vs 1. \nA continuación escoge el nombre y tipo de jugadores que se van a enfrentar a muerte.");
+
+        //Creación personaje 1
+        System.out.println("Escoge el nombre del personaje 1: ");
+
+        System.out.println("Escoge el tipo del personaje 1: ");
+
+
+        //Creación personaje 2
+        System.out.println("Escoge el nombre del personaje 2: ");
+
+        System.out.println("Escoge el tipo del personaje 2: ");
 
     }
 }
