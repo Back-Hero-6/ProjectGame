a785de839932c0f561c1955a1df0f4a6700423fa
vkap93 <victorkap@hotmail.com>
2022-11-16 22:32:07 +0100

    final

--- GameJava/src/Main.java
+++ GameJava/src/Main.java
@@ -1,7 +1,5 @@
-import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
