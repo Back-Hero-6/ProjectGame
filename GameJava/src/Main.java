54c70d292275f3f7657bce28b2dfcd3ded4093cc
vkap93 <victorkap@hotmail.com>
2022-11-17 00:48:14 +0100

    ff

--- GameJava/src/Main.java
+++ GameJava/src/Main.java
@@ -19,18 +19,18 @@ public class Main {
         System.out.println("╱┃┃╰╋╋╯┃┃┃╭━╮┃╰━╯┃╱┃┃╱╱╱┃┃╱┃┃╱╭┫╭━━╯┃┃╭━┫╰━╯┃┃┃┃┃┃╭━━╯");
         System.out.println("╭╯╰┳╋╋┳╯╰┫╰━╯┃╭━╮┃╱┃┃╱╱╱┃┃╱┃╰━╯┃╰━━╮┃╰┻━┃╭━╮┃┃┃┃┃┃╰━━╮");
         System.out.println("╰━━┻╯╰┻━━┻━━━┻╯╱╰╯╱╰╯╱╱╱╰╯╱╰━━━┻━━━╯╰━━━┻╯╱╰┻╯╰╯╰┻━━━╯");
-        System.out.println(blue+"Welcome to 1x1 Battle Game!" + "\n" + "To start a new battle you have to choose the type and name of both Characters!"+reset+"\n");
+        System.out.println(blue + "Welcome to 1x1 Battle Game!" + "\n" + "To start a new battle you have to choose the type and name of both Characters!" + reset + "\n");
 
         Scanner sc = new Scanner((System.in));
         Character character1 = null;
         System.out.println("First create the Character 1!");
         System.out.println("-------------------------");
         System.out.println("Character types available:");
-        System.out.println(green+"[1] -> Warrior");
-        System.out.println("[2] -> Wizard"+reset);
-        System.out.println("\u001B[31m"+"[3] -> Exit Game"+"\u001B[31m"+reset);
+        System.out.println(green + "[1] -> Warrior");
+        System.out.println("[2] -> Wizard" + reset);
+        System.out.println("\u001B[31m" + "[3] -> Exit Game" + "\u001B[31m" + reset);
         System.out.println();
-        System.out.println(red+"Choose the Character type by entering 1 or 2: "+reset);
+        System.out.println(red + "Choose the Character type by entering 1 or 2: " + reset);
 
         int options1 = 1;
 
@@ -41,7 +41,7 @@ public class Main {
 
                     System.out.println("You have chosen a warrior as character 1");
                     System.out.println("---------------");
-                    System.out.println(red+"Give this warrior a name:"+reset);
+                    System.out.println(red + "Give this warrior a name:" + reset);
 
                     String newName = sc.next();
                     int newHp = new Random().nextInt(101) + 100;
@@ -55,7 +55,7 @@ public class Main {
 
                     System.out.println("You have chosen the a wizard as character 1 ");
                     System.out.println("---------------");
-                    System.out.println(red+"Give this wizard a name:"+reset);
+                    System.out.println(red + "Give this wizard a name:" + reset);
 
                     String newName = sc.next();
                     int newHp = new Random().nextInt(51) + 50;
@@ -66,7 +66,7 @@ public class Main {
                     return character1;
                 }
                 case 3: {
-                    System.out.println("\n"+blue+"GOODBYE!"+reset);
+                    System.out.println("\n" + blue + "GOODBYE!" + reset);
                     System.out.println("---------------");
                     System.exit(1);
                 }
@@ -86,12 +86,12 @@ public class Main {
         System.out.println("Now create the second character!");
         System.out.println("---------------");
         System.out.println("Character types available:");
-        System.out.println(green+"[1] -> Warrior");
-        System.out.println("[2] -> Wizard"+reset);
-        System.out.println("\u001B[31m"+"[3] -> Exit Game"+"\u001B[31m"+reset);
+        System.out.println(green + "[1] -> Warrior");
+        System.out.println("[2] -> Wizard" + reset);
+        System.out.println("\u001B[31m" + "[3] -> Exit Game" + "\u001B[31m" + reset);
         System.out.println();
 
-        System.out.println(red+"Choose the Character type by entering 1 or 2:"+reset);
+        System.out.println(red + "Choose the Character type by entering 1 or 2:" + reset);
 
         int options2 = 1;
 
@@ -100,9 +100,9 @@ public class Main {
             switch (options2) {
                 case 1: {
 
-                    System.out.println(green+"You have chosen a warrior as character 2"+reset);
+                    System.out.println(green + "You have chosen a warrior as character 2" + reset);
                     System.out.println("---------------");
-                    System.out.println(red+"Give this warrior a name:"+reset);
+                    System.out.println(red + "Give this warrior a name:" + reset);
 
                     String newName = sc.next();
                     int newHp = new Random().nextInt(101) + 100;
@@ -115,9 +115,9 @@ public class Main {
                 }
                 case 2: {
 
-                    System.out.println(green+"You have chosen a wizard as character 2"+reset);
+                    System.out.println(green + "You have chosen a wizard as character 2" + reset);
                     System.out.println("---------------");
-                    System.out.println(red+"Give this wizard a name:"+reset);
+                    System.out.println(red + "Give this wizard a name:" + reset);
 
                     String newName = sc.next();
                     int newHp = new Random().nextInt(51) + 50;
@@ -128,7 +128,7 @@ public class Main {
                     return character2;
                 }
                 case 3: {
-                    System.out.println("\n"+blue+"GOODBYE!"+reset);
+                    System.out.println("\n" + blue + "GOODBYE!" + reset);
                     System.out.println("---------------");
                     System.exit(1);
                 }
@@ -144,39 +144,39 @@ public class Main {
         int counter = 1;
         int init_hp1 = character1.getHp();
         int init_hp2 = character2.getHp();
-        System.out.println("\n"+blue+ "BOTH CHARACTERS SELECTED:"+reset);
+        System.out.println("\n" + blue + "BOTH CHARACTERS SELECTED:" + reset);
         System.out.println(character1);
         System.out.println(character2 + "\n");
         System.out.println("Preparing battle...");
         System.out.println("------------------------" + "\n");
         Thread.sleep(3000);
         do {
-            System.out.println(green+"ROUND " + counter + " starts:" + "\n"+reset);
+            System.out.println(green + "ROUND " + counter + " starts:" + "\n" + reset);
             character1.attack(character2);
             character2.attack(character1);
-            System.out.println(red+"Round " + counter + " finalized:"+reset + "\n" + character1 + "\n" + character2 + "\n");
+            System.out.println(red + "Round " + counter + " finalized:" + reset + "\n" + character1 + "\n" + character2 + "\n");
             System.out.println("------------------------" + "\n");
             counter++;
             Thread.sleep(1000);
         } while (character1.isAlive() && character2.isAlive());
         if (character1.isAlive()) {
-            System.out.println(green+character1.getName() + " wins!"+reset+"\n");
-            System.out.println(red+"Battle finalized: Choose your characters again or exit game"+reset+"\n");
-            System.out.println("...resetting game..."+"\n");
+            System.out.println(green + character1.getName() + " wins!" + reset + "\n");
+            System.out.println(red + "Battle finalized: Choose your characters again or exit game" + reset + "\n");
+            System.out.println("...resetting game..." + "\n");
             System.out.println("----------------------------------------------------------------------------");
             Thread.sleep(3000);
             battle(createFirstCharacter(), createSecondCharacter());
         } else if (character2.isAlive()) {
-            System.out.println(green+character2.getName() + " wins!"+reset+"\n");
-            System.out.println(red+"Battle finalized: Choose your characters again or exit game"+reset+"\n");
-            System.out.println("...resetting game..."+"\n");
+            System.out.println(green + character2.getName() + " wins!" + reset + "\n");
+            System.out.println(red + "Battle finalized: Choose your characters again or exit game" + reset + "\n");
+            System.out.println("...resetting game..." + "\n");
             System.out.println("----------------------------------------------------------------------------");
             Thread.sleep(3000);
             battle(createFirstCharacter(), createSecondCharacter());
         } else {
-            System.out.println(red+"There was a Draw!"+reset+"\n");
-            System.out.println(red+"Battle finalized: Wait while new battle is initialized"+reset+"\n");
-            System.out.println("...generating new battle..."+"\n");
+            System.out.println(red + "There was a Draw!" + reset + "\n");
+            System.out.println(red + "Battle finalized: Wait while new battle is initialized" + reset + "\n");
+            System.out.println("...generating new battle..." + "\n");
             System.out.println("----------------------------------------------------------------------------");
             character1.setHp(init_hp1);
             character2.setHp(init_hp2);
